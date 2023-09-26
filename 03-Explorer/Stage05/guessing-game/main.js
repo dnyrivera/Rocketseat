8ba94545aae441045d9d0d255ff68103c1a50096
Donny Rivera <donny.rivera@gmail.com>
2023-09-25 22:55:46 -0400

    feat(JS):Guessing Game

--- 03-Explorer/Stage05/guessing-game/main.js
+++ 03-Explorer/Stage05/guessing-game/main.js
@@ -1,19 +1,38 @@
 const randomNumber = Math.round(Math.random() * 10);
+const screen01 = document.querySelector(".screen01");
+const screen02 = document.querySelector(".screen02");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
 let xAttempts = 1;
 
 console.log(randomNumber);
 
-function handleClick(event) {
+//Events
+btnTry.addEventListener("click", handleClickTry);
+btnReset.addEventListener("click", handleClickReset);
+
+//Functions
+function handleClickTry(event) {
 	event.preventDefault();
 
 	const inputNumber = document.querySelector("#inputNumber");
+	const h2Text = document.querySelector(".screen02 h2");
 	const finalText = `You Guessed it in ${xAttempts} attempts`;
 
 	if (Number(inputNumber.value) == randomNumber) {
-		document.querySelector(".screen01").classList.add("hide");
-		document.querySelector(".screen02").classList.remove("hide");
-		document.querySelector(".screen02 h2").innerText = finalText;
+		toggleAttributes();
+		h2Text.innerText = finalText;
 	}
-	
+	inputNumber.value = "";
 	xAttempts++;
+	// console.log(xAttempts);
+}
+function handleClickReset(event) {
+	toggleAttributes();
+	xAttempts = 1;
+}
+function toggleAttributes() {
+	screen01.classList.toggle("hide");
+	screen02.classList.toggle("hide");
 }
